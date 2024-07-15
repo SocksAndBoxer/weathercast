e4927737b3a10e464ca8e7e6a0184c271a3ee636
Guillaume Marc <marcguillaume77@gmail.com>
2024-07-15 18:33:06 +0200

    Add tailwind and update css

--- src/components/DailyForecast.tsx
+++ src/components/DailyForecast.tsx
@@ -3,21 +3,23 @@ import { degreesToDirection } from '../utils/degrees-to-direction'
 
 type DailyForecastProps = {
   day: TForecast
+  handleHourlyForecast: (hourly: THourly[]) => void
 }
 
-const DailyForecast = ({ day }: DailyForecastProps) => {
-  const handleHourlyForecast = (hourly: THourly[]) => {
-    console.log(hourly)
-  }
-
+const DailyForecast = ({ day, handleHourlyForecast }: DailyForecastProps) => {
   return (
-    <div onClick={() => handleHourlyForecast(day.hourly)}>
-      <h3>{day.date!.toLocaleDateString('en', { weekday: 'long' })}</h3>
-      <p>
-        {Math.round(day.minTemp)}° - {Math.round(day.maxTemp)}°
-      </p>
-      <p>Wind direction: {degreesToDirection(day.windDirection)}</p>
-    </div>
+    <>
+      <div
+        className='cursor-pointer'
+        onClick={() => handleHourlyForecast(day.hourly)}
+      >
+        <h3>{day.date!.toLocaleDateString('en', { weekday: 'long' })}</h3>
+        <p>
+          {Math.round(day.minTemp)}° - {Math.round(day.maxTemp)}°
+        </p>
+        <p>Wind direction: {degreesToDirection(day.windDirection)}</p>
+      </div>
+    </>
   )
 }
 
